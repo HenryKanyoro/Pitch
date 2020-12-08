e3f0b600ec884438516792b811c206787a9492c4
HenryKanyoro <kanyorohenry6@gmail.com>
2020-12-08 09:26:04 +0300

    changes on email.py

--- app/email.py
+++ app/email.py
@@ -5,7 +5,7 @@ from . import mail
 
 
 def mail_message(subject,template,to,**kwargs):
-    subject_pref = 'Pitches'
+    subject_pref = 'Pitches
     sender_email = 'henrykanyoro9@gmail.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
