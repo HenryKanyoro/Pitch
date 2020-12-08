f3c5e6434f900d023f647d5c8dd2920c7445525e
HenryKanyoro <kanyorohenry6@gmail.com>
2020-12-08 09:02:40 +0300

    changes on view.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,7 +23,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("main.index"))
+    return redirect(url_for("main.index")
     
 @auth.route('/register', methods=["GET", "POST"])
 def register():
